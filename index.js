38853db609fb961a2badc3b379e99a980ac8b664
Nivas Donthireddy <dlnivas@hotmail.com>
2022-11-01 09:06:47 +0530

    get request

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res,next)=>{
+    res.status(200).send('Hi , Welcome to Energy Reflux!');
+})
+
 app.listen(PORT,()=>{
     console.log(`Server Listening on port: ${PORT}`);
 })
